bfca06fcf894a41318f53fd0415e8322496eef01
mohammad <m.deheghanifard@gmail.com>
2024-05-06 09:37:07 +0330

    impl updateCategory api

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers;
 
 use App\Models\Post;
-use Illuminate\Contracts\Validation\ValidatorAwareRule;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
